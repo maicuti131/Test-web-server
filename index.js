5afc244c11710ff02404734d76abeed5a736559f
maicuti131 <ng.thanhthao0207e@gmail.com>
2025-05-15 17:27:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send(./index.html);
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
